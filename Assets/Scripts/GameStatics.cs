151ab93359cfe9ce9d2f0e9e89c9c041d8985b7a
Ömer Şahin <omersahin0309@gmail.com>
2024-02-24 01:55:58 +0300

    Update

--- Assets/Scripts/GameStatics.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GameStatics
-{
-    public static int SceneCount = 2;
-    public static string ButtonPosition = "Right";
-}
